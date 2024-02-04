9ff32a582b7397a11f482e9314480cf73597d57d
Nicolás <con23197@uvg.edu.gt>
2024-02-04 17:08:41 -0600

    Creacion de clases y metodos de clase orden

---
+++ GeneradorD.java
@@ -0,0 +1,23 @@
+public class GeneradorD {
+
+    public static void gnomeSort(int[] arr) {
+        // Implementación del algoritmo Gnome Sort
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        // Implementación del algoritmo Merge Sort
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        // Implementación del algoritmo Quick Sort
+    }
+
+    public static void radixSort(int[] arr) {
+        // Implementación del algoritmo Radix Sort
+    }
+
+    // Método adicional para el algoritmo de sort nuestra elección
+    public static void customSort(int[] arr) {
+        // Implementación de un algoritmo 
+    }
+}
---
+++ Orden.java
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class Orden {
+
+    // Método para generar un arreglo de números aleatorios
+    public static int[] generarNumerosAleatorios(int cantidad) {
+        Random rand = new Random();
+        int[] arr = new int[cantidad];
+        for (int i = 0; i < cantidad; i++) {
+            arr[i] = rand.nextInt(1000); // Genera un número aleatorio entre 0 y 999
+        }
+        return arr;
+    }
+
+    // Método para escribir el arreglo en un archivo
+    public static void escribirArchivo(String nombreArchivo, int[] arr) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo))) {
+            for (int num : arr) {
+                bw.write(num + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
