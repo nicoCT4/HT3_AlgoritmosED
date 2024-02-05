09f9631d309d03a96c1a35b87aa02b9ede2a2ef6
Fernando Hernandez <ferandree270904@gmail.com>
2024-02-05 09:25:43 -0600

    Update GeneradorD
    Se agrego quickSort y partition

--- GeneradorD.java
+++ GeneradorD.java
@@ -82,6 +82,38 @@ public class GeneradorD {
         }
     }
     
-
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            // partitionIndex es el índice de partición, arr[partitionIndex] ya está en el lugar correcto
+            int partitionIndex = partition(arr, low, high);
     
+            // Ordenar recursivamente los elementos antes y después de la partición
+            quickSort(arr, low, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, high);
+        }
+    }
+    
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low-1); // índice del elemento más pequeño
+        for (int j=low; j<high; j++) {
+            // Si el elemento actual es menor que el pivote
+            if (arr[j] < pivot) {
+                i++;
+    
+                // intercambiar arr[i] y arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    
+        // intercambiar arr[i+1] y arr[high] (o pivote)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+    
+        return i+1;
+    }
+
 }
