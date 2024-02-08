25442cd24a5845c9bdda044e00cb8885f0174165
Fernando Hernandez <ferandree270904@gmail.com>
2024-02-07 20:43:13 -0600

    Cambio de inicio

--- Driver.java
+++ Driver.java
@@ -22,9 +22,13 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
+        Scanner Scanner= new Scanner(System.in);
+        System.out.println("Presione cualquier tecla para iniciar");
+        String tecla = Scanner.nextLine();
         // Generar 3000 números aleatorios
         Integer[] numerosAleatorios = Arrays.stream(Orden.generarNumerosAleatorios(3000)).boxed().toArray(Integer[]::new);
 
@@ -82,9 +86,3 @@ public class Driver {
         }
     }
 }
-
-
-
-
-
-
